3766750ff5ff95061a70c58c4b0f43c3edc8ebe6
25LP8771 25LP8771 <25LP8771@admins-iMac.local>
2026-01-22 17:32:27 +0800

    fix

--- src/app/success/page.js
+++ src/app/success/page.js
@@ -3,8 +3,8 @@ export default function Home() {
   return (
     <div>
       <div className="w-[480px] h-[193px] border rounded-[5px] bg-white flex flex-col gap-[8px]">
-        <img src="Main 1.png" className="w-[60px] h-[60px]" />
-        <p className="font-black text-3xl">You're All Set 🔥</p>
+        <img src="Main 1.png" className="w-[60px] h-[60px]" alt="icon" />
+        <p className="font-black text-3xl">{"You're All Set 🔥"}</p>
         <p className="text-[#8E8E8E]">
           We have received your submission. Thank you!
         </p>
